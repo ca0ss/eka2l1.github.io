d15891071301459d8ca87cae1778fb2648d5fd70
pent0 <pent0@users.noreply.github.com>
2021-01-27 02:20:55 +0000

    jekyll build from Action 7cf7dac883d2b8497b8595e85387c86094e50420

---
+++ pt-br/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Novo website!",
+        "excerpt":"Este é o nosso website agora. Você verá os novos anúncios aqui!  ","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "https://12z1.com/jekyll/update/2021/01/19/new-website.html",
+        "teaser": null
+      }]
